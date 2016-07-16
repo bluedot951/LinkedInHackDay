a90257bd48546b2b669bb14d29074f981e91fc20
skaundinya15 <skaundinya@Sanjanas-Macbook-Pro.local>
2016-07-16 11:43:37 -0700

    changed 10.0 to 100

--- hack/www/js/controllers.js
+++ hack/www/js/controllers.js
@@ -40,7 +40,7 @@ angular.module('starter.controllers', [])
 
 			console.log($scope.distances.selected);
 
-			var url = "http://placesrec.herokuapp.com/searchPlaces?lat=" + lat + "&long=" + long + "&category=" + $scope.category.selected.name + "&dist=" + $scope.distances.selected / 10.0 + "&sort=relevance/";
+			var url = "http://placesrec.herokuapp.com/searchPlaces?lat=" + lat + "&long=" + long + "&category=" + $scope.category.selected.name + "&dist=" + $scope.distances.selected / 100 + "&sort=relevance/";
 			console.log(url);
 
 			$http.get(url)
