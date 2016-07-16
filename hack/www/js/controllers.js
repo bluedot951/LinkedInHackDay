6592d9b30b13fe96f11925df9806af5c86ef048c
Shantanu Gore <gore.shantanu@gmail.com>
2016-07-16 05:54:20 -0700

    connecting fe and be

--- hack/www/js/controllers.js
+++ hack/www/js/controllers.js
@@ -36,6 +36,8 @@ angular.module('starter.controllers', [])
 			$http.get(url)
 			.then(function successCallback(response) {
 				$scope.data = response.data;
+				$rootScope.data = response.data;
+
 				console.log(response.data);
 			}, function errorCallback(response) {
 				console.log(response)
@@ -59,7 +61,7 @@ angular.module('starter.controllers', [])
 
     $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
 })
-.controller('RecommendationCtrl', function($scope, $ionicModal) {
+.controller('RecommendationCtrl', function($scope, $ionicModal, $http) {
     $scope.result = [{
         name: 'Temple',
         category: 'Nightclub',
@@ -77,8 +79,35 @@ angular.module('starter.controllers', [])
         $scope.modal = modal;
     });
   
-    $scope.addRecommendation = function(reco) {        
+    $scope.addRecommendation = function(reco) {   
+        console.log(reco.name);
         console.log(reco.location);
         console.log(reco.category);
+
+        var url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + $scope.loc + "&key=AIzaSyAqFzn85y_cm_0TKrGInkjVOEj_IgMyAFE";
+        console.log("QUERYING URL : " + url);
+
+        $http.get(url)
+		.then(function successCallback(response) {
+			long = response.data.results[0].geometry.viewport.northeast.lng;
+			lat = response.data.results[0].geometry.viewport.northeast.lat;
+			console.log(long);
+			console.log(lat);
+
+			var url = "http://placesrec.herokuapp.com/addPlace?name=" + reco.name + "&lat=" + lat + "&long=" + long + "&category=" + reco.category;
+			console.log(url);
+
+			$http.get(url)
+			.then(function successCallback(response) {
+				console.log(response.data);
+			}, function errorCallback(response) {
+				console.log(response)
+			})
+
+		}, function errorCallback(response) {
+			console.log("error occured");
+		});
+
+
     };
 });
